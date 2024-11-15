c9b19e11203753304d9eed69a28024d999627229
Balint Maschio <bmaschio@imolainformatica.it>
2024-11-15 12:23:47 +0100

    First commit

---
+++ src/main/java/balint/example/batchFileToFile/BatchFileToFileApplication.java
@@ -0,0 +1,13 @@
+package balint.example.batchFileToFile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchFileToFileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchFileToFileApplication.class, args);
+	}
+
+}
---
+++ src/test/java/balint/example/batchFileToFile/BatchFileToFileApplicationTests.java
@@ -0,0 +1,13 @@
+package balint.example.batchFileToFile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchFileToFileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
